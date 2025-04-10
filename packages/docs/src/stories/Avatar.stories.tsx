758ef9dfa43f342fa7cf54db1690fd92a3c572d0
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-04-10 10:37:53 -0300

    feat: adicionando controle no componente de avatar

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -5,6 +5,13 @@ export default {
   title: 'Data display/Avatar',
   component: Avatar,
   tags: ['autodocs'],
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<IAvatarProps>
 
 export const Primary: StoryObj<IAvatarProps> = {}
